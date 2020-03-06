ac843fa79286113ebefb03db2403a40d6f45ece0
Oleg <oleg.krutohh@andevis.ee>
2020-03-06 10:04:49 +0200

    fixed h2 configuration

--- src/test/java/bill/provider/ProviderApplicationTests.java
+++ src/test/java/bill/provider/ProviderApplicationTests.java
@@ -1,11 +1,9 @@
 package bill.provider;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -13,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class ProviderApplicationTests {
